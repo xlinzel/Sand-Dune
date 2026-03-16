be3e4254ee559da80a5b9d672fb9ba9465ad7573
CanadianMeese <xanderlinzel@gmail.com>
2026-03-15 22:20:45 -0400

    3/15/2026: Reconstruction comments and plan

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/xlinzel/Desktop/BOS/Sand Dune
+# Install script for directory: C:/Users/Eclipse/OneDrive/Desktop/Background Oriented Schlieren
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/xlinzel/Desktop/BOS/Sand Dune/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/Eclipse/OneDrive/Desktop/Background Oriented Schlieren/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/xlinzel/Desktop/BOS/Sand Dune/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Eclipse/OneDrive/Desktop/Background Oriented Schlieren/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- include/grains/reconstruction.h
+++ include/grains/reconstruction.h
@@ -1,8 +1,32 @@
 #pragma once
 
+#include <grains/reconstruction.h>
+#include <wind/vectorfield.h>
+
 //https://cpb.iphy.ac.cn/article/2017/1892/cpb_26_6_064701.html
 
- class Reconstruction
- {
+//Most important (FFT method, not technically the best)
+//https://wavepy.readthedocs.io/en/latest/source/api/wavepy.surface_from_grad.html
 
- };
+//MATLAB Surface reconstruction sources
+//https://ieeexplore.ieee.org/document/5995427
+//https://ieeexplore.ieee.org/document/4587414
+//https://arxiv.org/abs/1308.4292
+//https://www.researchgate.net/publication/259099431_Direct_regularized_surface_reconstruction_from_gradients_for_Industrial_Photometric_Stereo
+
+
+
+class Reconstruction
+{
+public:
+    Reconstruction();
+
+    //Full pipeline
+    Eigen::MatrixXf Compute(const VectorField& data) const;
+
+private:
+    //Sub steps???
+
+    //Apply circular Hanning window???
+
+};
